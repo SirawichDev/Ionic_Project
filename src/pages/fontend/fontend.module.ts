70f7dc9ba1885c32085e37007daf3fa0a1dab465
SirawichDev <sirawit0676@gmail.com>
2018-03-19 21:56:23 +0700

    ionic project1 added

---
+++ src/pages/fontend/fontend.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { FontendPage } from './fontend';
+
+@NgModule({
+  declarations: [
+    FontendPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(FontendPage),
+  ],
+})
+export class FontendPageModule {}
