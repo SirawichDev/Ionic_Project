efb1eb303cfdd960a8bb41142d20982f3cfc666f
SirawichDev <sirawit0676@gmail.com>
2018-03-27 23:58:59 +0700

    intent to Shoppy page

--- src/pages/Shoppy/Shop.ts
+++ src/pages/Shoppy/Shop.ts
@@ -1,12 +1,11 @@
-import { NavController } from 'ionic-angular';
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'page-shoppy',
-  templateUrl: 'Shop.html'
+  templateUrl: 'Shop.html',
 })
 export class GotoShoppy {
-  constructor (private navCtrl: NavController){}
 
 }
 
