2d86022d4679dca12560e77c345cac013ce53b25
SirawichDev <sirawit0676@gmail.com>
2018-03-29 08:04:59 +0700

    slide 1 not finish yet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
+import { IntroPage } from './../pages/intro/intro';
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = IntroPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
